3bfb4172ec3d3d15f62c40fbfe43b2858736e949
Guilherme Barbieri <93358510+Guibarbieri2006@users.noreply.github.com>
2022-08-11 23:15:35 -0300

    Add files via upload

---
+++ IDG-Sigma/includes/_header.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+    <img src="./content/logo-ic1.png">
+        
+        <a class="navbar-brand" href="#">Ídolos Do Gigante Sigma</a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./produtos.php">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./contato.php">Contato</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<?php
+include_once './includes/_banco.php';
+?>
